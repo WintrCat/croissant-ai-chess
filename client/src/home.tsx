b3b319c95b60c81f574cc79a2cff0d4235cb39be
The Brigade <wintrcat.wilson@gmail.com>
2026-02-08 03:35:16 +0000

    feat: add chess board and some buttons

--- client/src/home.tsx
+++ client/src/home.tsx
@@ -1,5 +1,28 @@
+import { Button, Group } from "@mantine/core";
+import { Chessboard } from "react-chessboard";
+
+import styles from "./home.module.css";
+
 function Home() {
-    return <h1>The Best Home</h1>;
+    return <div className={styles.wrapper}>
+        <span className={styles.title}>
+            AI Piece Chess
+        </span>
+
+        <Chessboard options={{
+            boardStyle: { width: "min(700px, 100%)" }
+        }}/>
+
+        <Group>
+            <Button>
+                Start Game
+            </Button>
+
+            <Button color="red">
+                Undo Move
+            </Button>
+        </Group>
+    </div>;
 }
 
 export default Home;
--- client/src/root.tsx
+++ client/src/root.tsx
@@ -1,15 +1,20 @@
+import { MantineProvider } from "@mantine/core";
 import { Outlet, Scripts, ScrollRestoration } from "react-router";
+
+import "@mantine/core/styles.css";
 
 function App() {
     return <html>
         <head>
-            <title>The Best App</title>
+            <title>AI Chess</title>
 
             <link rel="icon" href="/favicon.ico"/>
         </head>
 
         <body>
-            <Outlet/>
+            <MantineProvider>
+                <Outlet/>
+            </MantineProvider>
 
             <ScrollRestoration/>
             <Scripts/>
